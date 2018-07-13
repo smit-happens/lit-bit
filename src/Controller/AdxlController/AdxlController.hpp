e1b1789b30b7d6f946787f5ae4a60231686a54db
Smitty <asmit147@ycp.edu>
2018-07-13 11:46:35 -0400

    wut

---
+++ src/Controller/AdxlController/AdxlController.hpp
@@ -0,0 +1,41 @@
+/**  Adxl controller class
+ *
+ * #include "AdxlController.hpp"
+ * Created 06-02-18 By: Smitty
+ *
+ * A longer description.
+ */
+
+#ifndef ADXLCONTROLLER_HPP
+#define ADXLCONTROLLER_HPP
+
+#include "../BaseController/BaseController.hpp"
+#include "../../Model/Adxl/Adxl.hpp"
+
+
+class AdxlController : public BaseController
+{
+public:
+    Adxl* adxlModel;
+
+    ~AdxlController(void);
+    static AdxlController*   getInstance();
+    void init(void);
+
+    void getInterruptSource(void);
+
+    int getMagnitude(void);
+    void printValues();
+
+private:
+    //Private contstructor so that it can't be called
+    AdxlController() {};
+    //copy constructor is private
+    AdxlController(AdxlController const&) {};
+    //static instance pointer
+    static AdxlController* _pInstance;
+
+};
+
+
+#endif  //ADXLCONTROLLER_HPP
