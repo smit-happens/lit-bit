0396b13bcc398c9cb1af8b20e6b7a9aa10de7ce5
Smitty <asmit147@ycp.edu>
2018-07-23 15:45:40 -0400

    modified to have pat's port file

--- src/Hardware/Constants/Port.hpp
+++ src/Hardware/Constants/Port.hpp
@@ -43,61 +43,65 @@ const uint8_t LB_SPI_MOSI         = MOSI;  //
 const uint8_t LB_SPI_MISO         = MISO;  //
 
 
-/*
-     _    ______  ___
-    / \  |  _ \ \/ / |
-   / _ \ | | | \  /| |
-  / ___ \| |_| /  \| |___
- /_/   \_\____/_/\_\_____|
+// /*
+//      _    ______  ___
+//     / \  |  _ \ \/ / |
+//    / _ \ | | | \  /| |
+//   / ___ \| |_| /  \| |___
+//  /_/   \_\____/_/\_\_____|
 
-*/
-const uint8_t LB_ADXL_INT1        = 1;
-// const uint8_t LB_ADXL_INT2        = NAN;
-
-
-/*
-  ____ _____ ____
- |  _ \_   _/ ___|
- | |_) || || |
- |  _ < | || |___
- |_| \_\|_| \____|
-
-*/
-const uint8_t LB_RTC_MFP          = 0;
+// */
+// const uint8_t LB_ADXL_INT1        = 1;
+// // const uint8_t LB_ADXL_INT2        = NAN;
 
 
-/*
-  ____  _     _____
- | __ )| |   | ____|
- |  _ \| |   |  _|
- | |_) | |___| |___
- |____/|_____|_____|
+// /*
+//   ____ _____ ____
+//  |  _ \_   _/ ___|
+//  | |_) || || |
+//  |  _ < | || |___
+//  |_| \_\|_| \____|
 
-*/
-const uint8_t LB_BLE_RDY          = 7;
-const uint8_t LB_BLE_REQ          = 9;
-const uint8_t LB_BLE_ACT          = 11;
-const uint8_t LB_BLE_RST          = 12;
+// */
+// const uint8_t LB_RTC_MFP          = 0;
 
 
-/*
-   ___  _     _____ ____
-  / _ \| |   | ____|  _ \
- | | | | |   |  _| | | | |
- | |_| | |___| |___| |_| |
-  \___/|_____|_____|____/
-*/
-//Data lines
-// SID:     Serial data out
-// SCLK:    Serial clock out
-// RS/A0:   Data/Command select
-// _RST:    LCD reset
-// _CS:     LCD chip select
-// const uint8_t LB_OLED_SID          = 8;   //ST7565 display Output
-// const uint8_t LB_OLED_SCLK         = 9;   //ST7565 display Output
-// const uint8_t LB_OLED_A0           = 10;  //ST7565 display Output
-// const uint8_t LB_OLED_RST          = 11;  //ST7565 display Output
-// const uint8_t LB_OLED_CS           = 12;  //ST7565 display Output
+// /*
+//   ____  _     _____
+//  | __ )| |   | ____|
+//  |  _ \| |   |  _|
+//  | |_) | |___| |___
+//  |____/|_____|_____|
+
+// */
+// const uint8_t LB_BLE_RDY          = 7;
+// const uint8_t LB_BLE_REQ          = 9;
+// const uint8_t LB_BLE_ACT          = 11;
+// const uint8_t LB_BLE_RST          = 12;
+
+
+/** 
+ * Accelerometer
+ */
+const uint8_t LB_ADXL_INT1 = 10;     // uP pin 30, Arduino/ItsyBitsy pin D10
+const uint8_t LB_ADXL_INT2 = 9;      // uP pin 29, Arduino/ItsyBitsy pin D9
+
+/** 
+ * Bluetooth
+ */
+const uint8_t LB_BLE_ACT = 11;      // uP pin 12, Arduino/ItsyBitsy pin D11
+const uint8_t LB_BLE_REQ = 7;         // uP pin 1, Arduino/ItsyBitsy pin D7
+const uint8_t LB_BLE_RDY = 0;         // uP pin 20, Arduino/ItsyBitsy pin D0
+//FIXME: commented code should work for PCB, below is for breadboard...
+// const uint8_t LB_BLE_RST = 5;
+const uint8_t LB_BLE_RST = RXLED0;     // uP pin 8, Arduino/ItsyBitsy pin "RXLED"
+
+// EEPROM (NONE)
+
+/** 
+ * RTC
+ */
+const uint8_t LB_RTC_MFP = 1;          // uP pin 21, Arduino/ItsyBitsy pin D1
 
 
 #endif  //PORTCONSTANTS_HPP
